b9a8a3003b162bf2354065fb0ce62b31271c0e20
ZcR-01 <zihan1048@live.com>
2024-11-13 17:19:38 +0100

    feat: change favicon and add portrait photo

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,12 +1,155 @@
-import React from "react";
-import { useTranslations } from "next-intl";
+"use client";
+
+import {
+  Box,
+  Button,
+  Divider,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import Cookies from "js-cookie";
+import portrait from "../../../public/illustration.png";
 
 function App() {
-  const t = useTranslations("HomePage");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const router = useRouter();
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSignInButton = async () => {
+    const response = await axios.post("http://127.0.0.1:5001/login", {
+      email: email,
+      password: password,
+    });
+
+    const expirationTime = new Date(
+      new Date().getTime() + 24 * 60 * 60 * 1000 // 24 hours
+    );
+
+    Cookies.set("accessToken", response.data.access_token, {
+      expires: expirationTime,
+      path: "/",
+    });
+
+    Cookies.set("email", email, {
+      expires: expirationTime,
+      path: "/",
+    });
+    Cookies.set("user_id", response.data.user_id, {
+      expires: expirationTime,
+      path: "/",
+    });
+
+    Cookies.set("user_id", response.data.user_id, {
+      expires: expirationTime,
+      path: "/",
+    });
+
+    if (response) {
+      router.push("/home");
+    }
+  };
+
   return (
-    <div className="App">
-      <h1>{t("title")}</h1>
-    </div>
+    <Grid container sx={{ minHeight: "100vh" }}>
+      <Grid
+        item
+        xs={12}
+        md={6}
+        sx={{ backgroundColor: "#ff2f2f", color: "#fff", p: 4 }}
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          height="100%"
+          textAlign="center"
+        >
+          <Image src={portrait} alt="Illustration" width={512} height={512} />
+          <Typography variant="h4" fontWeight="bold" mb={2}>
+            PetConnecting: Connect with Pet
+          </Typography>
+          <Typography variant="body1">
+            All your pet&apos;s needs in one place.
+          </Typography>
+        </Box>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        md={6}
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+        backgroundColor="#ffffff"
+      >
+        <Box sx={{ maxWidth: "400px", width: "100%", p: 4 }}>
+          <Typography
+            variant="h5"
+            fontWeight="bold"
+            mb={3}
+            align="center"
+            sx={{
+              color: "#000000",
+            }}
+          >
+            Log in
+          </Typography>
+          <TextField
+            label="Email"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{
+              mt: 2,
+              backgroundColor: "#ff2f2f",
+              color: "#fff",
+              "&:hover": {
+                backgroundColor: "#ff2f2f",
+              },
+            }}
+            onClick={handleSignInButton}
+          >
+            Log in
+          </Button>
+          <Divider sx={{ my: 3 }} />
+          <Typography variant="body2" sx={{ mt: 2, color: "#000000", ml: 5 }}>
+            Don&apos;t have an account?{" "}
+            <Link href="/signup" sx={{ color: "#ff3b30" }}>
+              Create account
+            </Link>
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
--- frontend/src/app/[locale]/signin/page.tsx
+++ frontend/src/app/[locale]/signin/page.tsx
@@ -14,15 +14,8 @@ import {
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import Cookies from "js-cookie";
-
-// const Logo = (props) => (
-//   <SvgIcon {...props} viewBox="0 0 24 24">
-//     <path d="M12,2C6.47,2,2,6.47,2,12s4.47,10,10,10s10-4.47,10-10S17.53,2,12,2z M12,20c-4.41,0-8-3.59-8-8s3.59-8,8-8s8,3.59,8,8 S16.41,20,12,20z" />
-//     <path d="M15.5,11c0.83,0,1.5-0.67,1.5-1.5S16.33,8,15.5,8S14,8.67,14,9.5S14.67,11,15.5,11z" />
-//     <path d="M8.5,11c0.83,0,1.5-0.67,1.5-1.5S9.33,8,8.5,8S7,8.67,7,9.5S7.67,11,8.5,11z" />
-//     <path d="M12,14c-2.33,0-4.31,1.46-5.11,3.5h10.22C16.31,15.46,14.33,14,12,14z" />
-//   </SvgIcon>
-// );
+import Image from "next/image";
+import logo from "../../../../public/logo.png";
 
 const SignInSide: React.FC = () => {
   const [email, setEmail] = React.useState<string>("");
@@ -83,8 +76,12 @@ const SignInSide: React.FC = () => {
       }}
     >
       <Box sx={{ mb: 4, display: "flex", alignItems: "center" }}>
-        {/* <Logo sx={{ fontSize: 40, color: "#ff3b30", mr: 1 }} /> */}
-        <Typography variant="h4" component="h1" sx={{ fontWeight: "bold" }}>
+        <Image src={logo} height={50} width={50} alt="PetCareConnect" />
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{ fontWeight: "bold", color: "#000000", marginLeft: 2 }}
+        >
           PetCareConnect
         </Typography>
       </Box>
@@ -165,94 +162,6 @@ const SignInSide: React.FC = () => {
         </Link>
       </Typography>
     </Box>
-    // <Grid
-    //   container
-    //   component="main"
-    //   sx={{ height: "100vh", backgroundColor: "white" }}
-    // >
-    //   <CssBaseline />
-    //   <Grid
-    //     item
-    //     xs={12}
-    //     sm={8}
-    //     md={5}
-    //     component={Paper}
-    //     elevation={6}
-    //     square
-    //     sx={{
-    //       display: "flex",
-    //       flexDirection: "column",
-    //       alignItems: "center",
-    //       justifyContent: "center",
-    //       padding: 4,
-    //     }}
-    //   >
-    //     <Box
-    //       sx={{
-    //         display: "flex",
-    //         flexDirection: "column",
-    //         alignItems: "center",
-    //         width: "100%",
-    //       }}
-    //     >
-    //       <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-    //         <LockOutlinedIcon />
-    //       </Avatar>
-    //       <Typography component="h1" variant="h5">
-    //         Sign in
-    //       </Typography>
-    //       <Box component="form" noValidate sx={{ mt: 1, width: "100%" }}>
-    //         <TextField
-    //           variant="outlined"
-    //           margin="normal"
-    //           required
-    //           fullWidth
-    //           id="email"
-    //           label="Email Address"
-    //           name="email"
-    //           autoComplete="email"
-    //           autoFocus
-    //         />
-    //         <TextField
-    //           variant="outlined"
-    //           margin="normal"
-    //           required
-    //           fullWidth
-    //           name="password"
-    //           label="Password"
-    //           type="password"
-    //           id="password"
-    //           autoComplete="current-password"
-    //         />
-    //         <FormControlLabel
-    //           control={<Checkbox value="remember" color="primary" />}
-    //           label="Remember me"
-    //         />
-    //         <Button
-    //           type="submit"
-    //           fullWidth
-    //           variant="contained"
-    //           color="primary"
-    //           sx={{ mt: 3, mb: 2 }}
-    //         >
-    //           Sign In
-    //         </Button>
-    //         <Grid container>
-    //           <Grid item xs>
-    //             <Link href="#" variant="body2">
-    //               Forgot password?
-    //             </Link>
-    //           </Grid>
-    //           <Grid item>
-    //             <Link href="/signup" variant="body2">
-    //               {"Don't have an account? Sign Up"}
-    //             </Link>
-    //           </Grid>
-    //         </Grid>
-    //       </Box>
-    //     </Box>
-    //   </Grid>
-    // </Grid>
   );
 };
 
