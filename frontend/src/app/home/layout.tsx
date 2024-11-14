e3fdf3933cfaa91bb3dc480acce0dbce8fc51798
wissamamer <wissamamer2000@gmail.com>
2024-11-14 15:41:21 +0100

    add  code for the chat button works in all mobile pages

--- frontend/src/app/home/layout.tsx
+++ frontend/src/app/home/layout.tsx
@@ -1,17 +1,38 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavigationBar from "../components/appBar";
 import SideBar from "../components/bar-sidebar";
 import BottomBar from "../components/BottomBar";
+import Chat from "../components/chat";
 import { CssBaseline, Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const [isChatVisible, setIsChatVisible] = useState(!isMobile);
+
+  useEffect(() => {
+    if (isMobile) {
+      setIsChatVisible(false);
+    } else {
+      setIsChatVisible(true);
+    }
+  }, [isMobile]);
+
+  const toggleChat = () => {
+    if (isMobile) {
+      setIsChatVisible((prev) => !prev);
+    }
+  };
+
   return (
     <>
       <CssBaseline />
 
-      <Box sx={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 1300 ,}}>
+      <Box sx={{ position: "fixed", top: 0, left: 0, right: 0, zIndex: 1300 }}>
         <NavigationBar />
       </Box>
 
@@ -24,17 +45,25 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           minHeight: "100vh",
           pt: "64px",
           pb: "56px",
-          paddingTop:"40px",
+          paddingTop: "40px",
           overflow: "auto",
+          bgcolor: "#F5F8FA",
         }}
       >
         {children}
       </Box>
-      <Box
-        sx={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 1400 }}
-      >
-        <BottomBar />
-      </Box>
+      {isChatVisible && (
+        <Box>
+          <Chat />
+        </Box>
+      )}
+      {isMobile && (
+        <Box
+          sx={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 1400 }}
+        >
+          <BottomBar toggleChat={toggleChat} />
+        </Box>
+      )}
     </>
   );
 }
