8cbce47889b0133a2e64bfc3f2bf4ce6202f6b92
ZcR-01 <zihan1048@live.com>
2024-11-17 23:20:33 +0100

    feat: configured PWA (progressive web app)

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,23 @@
+import withPWA from "next-pwa";
+import composePlugins from "next-compose-plugins"; // Import next-compose-plugins
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true, // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
+};
 
-export default withNextIntl(nextConfig);
+const withPWAConfig = withPWA({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in the development environment
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+export default composePlugins([withNextIntl, withPWAConfig], nextConfig);
